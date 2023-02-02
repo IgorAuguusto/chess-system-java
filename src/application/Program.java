b6627c4d0cbb891fa06aed7731c863ec8b4f7ded
Igor Augusto <igoraugusto36s@gmail.com>
2023-02-02 05:30:04 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!!");
+		Position po = new Position(1,2);
+		po.setValues(2, 0);;
+		
+		System.out.println(po);
 	}
 
 }
