4b75e8e3f77772faac0e11d719e59e9121f7d6a8
Igor Augusto <igoraugusto36s@gmail.com>
2023-02-09 01:57:46 -0300

    Bishop

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -52,25 +52,25 @@ public class King extends ChessPiece {
 		}
 
 		// nw
-		p.setValues(position.getRow() -1, position.getColumn() -1);
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
 		// ne
-		p.setValues(position.getRow() -1, position.getColumn() +1);
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
 		// sw
-		p.setValues(position.getRow() +1, position.getColumn() -1);
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
 		// se
-		p.setValues(position.getRow() +1, position.getColumn() +1);
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
